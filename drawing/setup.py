e232f6ff3cbd16d83ee464d23dc9dd0a3f62f7cd
Nambi <simaonambi@gmail.com>
2024-11-28 23:34:32 +0000

    second commit

---
+++ drawing/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='drawing',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],  # Adicione dependências aqui, se necessário
+    description='Pacote de exemplo para desenho',
+    author='Seu Nome',
+    author_email='seu.email@example.com',
+    url='https://github.com/seu_usuario/drawing',  # Opcional
+)
